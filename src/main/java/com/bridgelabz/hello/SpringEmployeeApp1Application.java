4085f1a4a2950cc164b1c97baf3ed9f8218d4031
sreeja99 <srijagodishala@gmail.com>
2020-12-05 20:23:00 +0530

    Added:Hello Message

---
+++ src/main/java/com/bridgelabz/hello/SpringEmployeeApp1Application.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmployeeApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmployeeApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/empPayrollApp/SpringEmployeeApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.empPayrollApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmployeeApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
